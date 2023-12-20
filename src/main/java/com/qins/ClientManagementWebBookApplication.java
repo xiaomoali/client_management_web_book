a18c69303a21240815600b1a652bdeee7c73f6ae
qins <2941634984@qq.com>
2023-12-20 17:41:10 +0800

    te

---
+++ src/main/java/com/qins/ClientManagementWebBookApplication.java
@@ -0,0 +1,13 @@
+package com.qins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementWebBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagementWebBookApplication.class, args);
+	}
+
+}
