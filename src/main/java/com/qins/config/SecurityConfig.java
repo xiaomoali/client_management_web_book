e9817b011d5a46e8521686abedd31fe0c54bd247
qins <2941634984@qq.com>
2023-12-21 10:49:15 +0800

    te

--- src/main/java/com/qins/config/SecurityConfig.java
+++ src/main/java/com/qins/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.qins.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -11,11 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig implements WebMvcConfigurer {
 
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/index.html").setViewName("index");
 
+    @RequestMapping({"/","/index.html"})
+    public String index(){
+        return "index";
     }
 
 
--- src/main/java/com/qins/controller/SecurityController.java
+++ src/main/java/com/qins/controller/SecurityController.java
@@ -3,13 +3,16 @@ package com.qins.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
 
 @Controller
 @ResponseBody
 public class SecurityController {
 
+
     @RequestMapping ("/index")
-    public String login(){
+    public String login( ){
         return "index";
     }
 }
